c0bc40b6eb798ab019e1e5ddf8a8e453dc999809
Vyacheslav Polyansky <polsha13esma@mail.ru>
2024-06-22 07:37:49 +0300

    Добавьте файлы проекта.

---
+++ Parking.cs
@@ -0,0 +1,95 @@
+﻿
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+public class Parking
+  {
+  private List<Car> p_cars = new List<Car>();
+  private const int p_maxCars = 100;
+  public Car this[string number]
+  {
+    get
+    {
+      var car = p_cars
+      .FirstOrDefault(c => c.Number == number);
+      return car;
+    }
+  }
+  public Car this[ int position]
+  {
+    get
+    {
+      if (position < p_cars.Count)
+      {
+        return p_cars[position];
+      }
+      return null;
+    }
+    set
+    {
+      if(position < p_cars.Count)
+      {  p_cars[position] = value; }
+    }
+  }
+
+  public string Name {  get; set; }
+  public int Count => p_cars.Count;
+
+  public int Add(Car car)
+  {
+    if (car == null)
+    {
+      throw new ArgumentNullException(nameof(car), "Car is null");
+    }
+
+    if (p_cars.Count < p_maxCars) { 
+      p_cars.Add(car);
+    return p_cars.Count - 1; }
+
+    else
+    {
+      return -1;
+    }
+
+  }
+  public void GoOut(string number)
+  {
+    if (string.IsNullOrWhiteSpace(number))
+    {
+      throw new ArgumentNullException(nameof(number), "Number is null or empty");
+    }
+    var car = p_cars
+      .FirstOrDefault(c => c.Number == number); 
+    if (car != null) 
+    {
+      p_cars.Remove(car);
+    }
+  }
+
+  public async IAsyncEnumerable<Car> GetCarsAsync([EnumeratorCancellation] CancellationToken cancellationToken)
+  {
+    foreach (var car in p_cars)
+    {
+      if (cancellationToken.IsCancellationRequested)
+      {
+        yield break;
+      }
+      yield return car;
+    }
+  }
+
+ 
+  public async IAsyncEnumerable<string> GetCarNamesAsync([EnumeratorCancellation] CancellationToken cancellationToken)
+  {
+    foreach (var car in p_cars)
+    {
+      if (cancellationToken.IsCancellationRequested)
+      {
+        yield break;
+      }
+      yield return car.Name;
+      
+    }
+  }
+}
+
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+class Program
+{
+  static async Task Main(string[] args)
+  {
+    var cars = new List<Car>()
+        {
+            new Car(){Name = "Ford", Number = "A001AA01"},
+            new Car(){Name = "Toyota",Number = "A002AA02"},
+        };
+
+    var parking = new Parking();
+
+   
+    foreach (var car in cars)
+    {
+      parking.Add(car);
+    }
+
+
+    using (var cts = new CancellationTokenSource())
+    {
+      var token = cts.Token;
+      await foreach (var car in parking.GetCarsAsync(token))
+      {
+        Console.WriteLine(car);
+      }
+    }
+
+   
+    using (var cts = new CancellationTokenSource())
+    {
+      var token = cts.Token;
+      await foreach (var name in parking.GetCarNamesAsync(token))
+      {
+        Console.WriteLine($"Имя: {name}");
+      }
+    }
+
+    Console.WriteLine();
+
+
+    Console.WriteLine(parking["A001AA01"]?.Name);
+    Console.WriteLine(parking["A222AA02"]?.Name);
+
+    Console.WriteLine("Введите номер нового автомобиля");
+    var num = Console.ReadLine();
+
+   
+    parking[1] = new Car() { Name = "BMW", Number = num };
+    Console.WriteLine(parking[1]);
+
+    Console.ReadLine();
+  }
+}
